32ea238ea24c0c65e137f844f575d2175c823216
John <mrjohnishappy@gmail.com>
2023-04-04 05:04:19 -0400

    Working on index anim

--- js/load.js
+++ js/load.js
@@ -16,12 +16,16 @@ $(document).ready(function () {
 
 function print(val) { console.log(val) };
 
-$('body').scroll(function() { 
-    console.log('test') 
+function scrolled() {
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("hero");
     var xvalue = "center";
-    var factor = 0.5;
-    var yvalue = scrolltotop * factor;
-    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-});
+    var factor = 0.14;
+    //var yvalue = scrolltotop * factor * -1;
+    //target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+    var viewportHeight = $('html').height();
+    var viewportWidth = $('html').width();
+    var percentDown = (document.scrollingElement.scrollTop)/viewportHeight*2
+    console.log(percentDown)
+    target.style.backgroundPosition = xvalue + " " + -700*percentDown + "px";
+}
