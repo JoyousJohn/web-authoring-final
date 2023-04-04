165d1d17b668eeae445f0b45813bdf29cd1bd80b
John <mrjohnishappy@gmail.com>
2023-04-04 05:15:16 -0400

    Gave up. Temp solution for standard screenwidth I guess :/

--- js/load.js
+++ js/load.js
@@ -20,12 +20,14 @@ function scrolled() {
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("hero");
     var xvalue = "center";
-    var factor = 0.14;
-    //var yvalue = scrolltotop * factor * -1;
-    //target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+    var factor = 0.4;
+    var yvalue = scrolltotop * factor * -1;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
     var viewportHeight = $('html').height();
     var viewportWidth = $('html').width();
+    var picHeight = viewportWidth * 0.66;
+    console.log(picHeight)
     var percentDown = (document.scrollingElement.scrollTop)/viewportHeight*2
     console.log(percentDown)
-    target.style.backgroundPosition = xvalue + " " + -700*percentDown + "px";
+    //target.style.backgroundPosition = xvalue + " " + (percentDown*100 + -1*picHeight) + "px";
 }
